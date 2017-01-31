3c6efa79f027b056b89d8b0fc3ba7eae8ffed3fd
ImRamesh <ramesh.s@praniontech.com>
2017-01-31 10:53:57 +0530

    Init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mmmangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mmmangularApp
+ */
+angular.module('mmmangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
